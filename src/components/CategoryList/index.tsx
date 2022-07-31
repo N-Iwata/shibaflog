986be7089bd65e24b7e8f744c4db7d3f34e31a16
Nobuhito Iwata <nobuhito323@gmail.com>
2022-08-01 03:29:35 +0900

    Add Category list

---
+++ src/components/CategoryList/index.tsx
@@ -0,0 +1,37 @@
+import { Box, List, ThemeIcon, Title } from '@mantine/core'
+import { Pencil1Icon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+
+import { Category } from '@shibaflog/types'
+
+type Props = {
+  category: Category[]
+}
+const CategoryList = ({ category }: Props) => (
+  <Box>
+    <Title order={3} sx={{ backgroundColor: 'teal', color: 'white', padding: '4px 8px' }}>
+      カテゴリ
+    </Title>
+
+    <List
+      spacing='xs'
+      center
+      icon={
+        <ThemeIcon color='teal' size={24} radius='xl'>
+          <Pencil1Icon />
+        </ThemeIcon>
+      }
+      sx={{ marginTop: 16 }}
+    >
+      {category.map(({ id, name }) => (
+        <List.Item key={id}>
+          <Link href={`/category/${id}`}>
+            <a>{name}</a>
+          </Link>
+        </List.Item>
+      ))}
+    </List>
+  </Box>
+)
+
+export default CategoryList
