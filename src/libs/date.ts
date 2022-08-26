a3debe8a9c97651ff983812264fc4d8676f19601
Nobuhito Iwata <nobuhito323@gmail.com>
2022-08-26 21:54:35 +0900

    Fix date fns locale

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -1,4 +1,7 @@
 import { format } from 'date-fns'
+import { ja } from 'date-fns/locale'
 
-export const formatYearMonth = (date: string): string => format(new Date(date), 'yyyy_MM')
-export const formatYearMonthDay = (date: string): string => format(new Date(date), 'yyyy_MM_dd')
+export const formatYearMonth = (date: string): string =>
+  format(new Date(date), 'yyyy_MM', { locale: ja })
+export const formatYearMonthDay = (date: string): string =>
+  format(new Date(date), 'yyyy_MM_dd', { locale: ja })
