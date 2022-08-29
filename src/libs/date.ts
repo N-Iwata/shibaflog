95faad2e0ea9e7101010efe4c5dd3f166cccd556
Nobuhito Iwata <nobuhito323@gmail.com>
2022-08-30 00:54:56 +0900

    Use dayjs instead of date-fns

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -1,7 +1,15 @@
-import { format, parseISO } from 'date-fns'
-import { ja } from 'date-fns/locale'
+// import { format, parseISO } from 'date-fns'
+// import { ja } from 'date-fns/locale'
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 export const formatYearMonth = (date: string): string =>
-  format(parseISO(date), 'yyyy_MM', { locale: ja })
+  // format(parseISO(date), 'yyyy_MM', { locale: ja })
+  dayjs.utc(date).tz('Asia/Tokyo').format('YYYY-MM')
 export const formatYearMonthDay = (date: string): string =>
-  format(parseISO(date), 'yyyy_MM_dd', { locale: ja })
+  // format(parseISO(date), 'yyyy_MM_dd', { locale: ja })
+  dayjs.utc(date).tz('Asia/Tokyo').format('YYYY-MM-dd')
