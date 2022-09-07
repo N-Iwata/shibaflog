b69da314287c308c10ead170839ab2abe4549ec1
Nobuhito Iwata <nobuhito323@gmail.com>
2022-09-07 21:59:37 +0900

    Use ArticleCard to archive and category page

--- src/pages/category/[id].tsx
+++ src/pages/category/[id].tsx
@@ -1,8 +1,8 @@
-import { Stack, Title } from '@mantine/core'
+import { Grid, Title } from '@mantine/core'
 import { GetStaticProps, GetStaticPaths } from 'next'
 import Head from 'next/head'
 
-import VerticalArticleCard from '@shibaflog/components/Card/VerticalArticleCard'
+import ArticleCard from '@shibaflog/components/Card/ArticleCard'
 import Main from '@shibaflog/components/Layout/Main'
 import { getArchiveList } from '@shibaflog/libs/archive'
 import { client } from '@shibaflog/libs/client'
@@ -29,19 +29,19 @@ const CategoryId = ({ blog, categoryDetail, categoryList, archiveList }: Props) 
           ? `[ ${categoryDetail.name} ] カテゴリの記事一覧`
           : `[ ${categoryDetail.name} ] カテゴリの記事がありません`}
       </Title>
-      <Stack spacing='md'>
-        {blog.map(({ title, hero, categories, publishedAt, revisedAt, id }) => (
-          <VerticalArticleCard
-            key={id}
-            id={id}
-            title={title}
-            hero={hero}
-            categories={categories}
-            publishedAt={publishedAt}
-            revisedAt={revisedAt}
-          />
+      <Grid gutter={16}>
+        {blog.map(({ title, hero, categories, publishedAt, id }) => (
+          <Grid.Col xs={12} sm={6} md={4} key={id}>
+            <ArticleCard
+              id={id}
+              title={title}
+              hero={hero}
+              categories={categories}
+              publishedAt={publishedAt}
+            />
+          </Grid.Col>
         ))}
-      </Stack>
+      </Grid>
     </Main>
   </>
 )
