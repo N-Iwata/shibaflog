62c548db9cffc8edd41af9aba185ce803375e0ef
Nobuhito Iwata <nobuhito323@gmail.com>
2023-01-23 02:45:48 +0900

    Fix styles

--- src/components/Twitter/TwitterFollowButton.tsx
+++ src/components/Twitter/TwitterFollowButton.tsx
@@ -4,8 +4,8 @@ import { IconBrandTwitter } from '@tabler/icons'
 const TwitterFollowButton = (props: ButtonProps & React.ComponentPropsWithoutRef<'a'>) => (
   <Button
     component='a'
-    leftIcon={<IconBrandTwitter size={20} color='teal' />}
-    variant='light'
+    leftIcon={<IconBrandTwitter size={20} color='white' />}
+    variant='filled'
     color='teal'
     {...props}
   />
