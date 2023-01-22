4cc42bee30c026a4e00605e99336051b5d6e0401
Nobuhito Iwata <nobuhito323@gmail.com>
2023-01-22 18:16:45 +0900

    Add Seo component

---
+++ src/components/SEO/Seo.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Head from 'next/head'
+
+import { ROOT_URL } from '@shibaflog/const/url'
+
+type Props = {
+  title: string
+  description: string
+  image?: string
+}
+
+const Seo = ({ title, description, image }: Props) => (
+  <Head>
+    <title>{`Shibaflog | ${title}`}</title>
+    <meta name='description' content={description} />
+    <meta property='og:title' content={title} />
+    <meta property='og:description' content={description} />
+    <meta property='og:image' content={image || `${ROOT_URL}/ogp.png`} />
+    <meta name='twitter:card' content='summary_large_image' />
+    <link rel='icon' href='/favicon.ico' />
+  </Head>
+)
+
+export default Seo
