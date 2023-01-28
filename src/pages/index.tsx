3727fa43f236e1fbc970a628eec225fdea032649
Nobuhito Iwata <nobuhito323@gmail.com>
2023-01-28 12:08:26 +0900

    Remove heatmap

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,20 @@ import Main from '@shibaflog/components/Layout/Main'
 import Seo from '@shibaflog/components/SEO/Seo'
 import { getArchiveList } from '@shibaflog/libs/archive'
 import { client } from '@shibaflog/libs/client'
-import { getHeatMapList } from '@shibaflog/libs/heatMap'
-import { Blog, Category, Archive, HeatMap } from '@shibaflog/types'
+// import { getHeatMapList } from '@shibaflog/libs/heatMap'
+import { Blog, Category, Archive } from '@shibaflog/types'
 
 type Props = {
   blog: Blog[]
   categoryList: Category[]
   archiveList: Archive
-  heatMapList: HeatMap[]
+  // heatMapList: HeatMap[]
 }
 
-const Home = ({ blog, categoryList, archiveList, heatMapList }: Props) => (
+const Home = ({ blog, categoryList, archiveList }: Props) => (
   <>
     <Seo title='Home' description='Home page' />
-    <Main categoryList={categoryList} archiveList={archiveList} heatMapList={heatMapList}>
+    <Main categoryList={categoryList} archiveList={archiveList}>
       <Grid gutter={16}>
         {blog.map(({ title, hero, categories, publishedAt, id }) => (
           <Grid.Col xs={12} sm={6} md={4} key={id}>
@@ -41,14 +41,14 @@ export const getStaticProps: GetStaticProps = async () => {
   const allBlogData = await client.getList<Blog>({ endpoint: 'blog', queries: { limit: 3000 } })
   const categoryListData = await client.getList<Category>({ endpoint: 'categories' })
   const archiveListData = getArchiveList(allBlogData.contents)
-  const heatMapListData = getHeatMapList(allBlogData.contents)
+  // const heatMapListData = getHeatMapList(allBlogData.contents)
 
   return {
     props: {
       blog: allBlogData.contents,
       categoryList: categoryListData.contents,
       archiveList: archiveListData,
-      heatMapList: heatMapListData,
+      // heatMapList: heatMapListData,
     },
   }
 }
